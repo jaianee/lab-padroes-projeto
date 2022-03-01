a5fd0cea9a0fe98cf066126e9f6db3fa8a15a554
jaianee <jaiane.furtado@outlook.com>
2022-03-01 14:31:19 -0300

    first commit

---
+++ src/com/dio/SingletonLazy.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+// Singleton preguiçoso
+
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+
+    }
+}
